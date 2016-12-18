c397535ed1a7137d494d80af14e02f4d4d774fa9
BrightSkyz <BrightSkyz@users.noreply.github.com>
2016-12-18 17:37:00 -0500

    Fixed column width on desktop

--- pages/addnode.php
+++ pages/addnode.php
@@ -7,23 +7,23 @@ Here is how to add a node:<br>
 &bull; Leave software open and cofigure the file below.
 </p>
 <div class="form-group">
-   <label class="col-lg-2 control-label">Name the node</label>
+   <label class="control-label">Name the node</label>
    <input type="text" class="form-control" placeholder="Node Name (Ex, FRANCE/Node1)">
 </div>
 <div class="form-group">
-   <label class="col-lg-2 control-label">Type the name of the user that will have the node.</label>
+   <label class="control-label">Type the name of the user that will have the node.</label>
    <input type="text" class="form-control" placeholder="Username">
 </div>
 <div class="form-group">
-   <label class="col-lg-2 control-label">Public IP of the node</label>
+   <label class="control-label">Public IP of the node</label>
    <input type="text" class="form-control" placeholder="12.34.567.89">
 </div>
 <div class="form-group">
-   <label class="col-lg-2 control-label">Type the code displayed when application is launched.</label>
+   <label class="control-label">Type the code displayed when application is launched.</label>
    <input type="text" class="form-control" placeholder="ys73j93">
 </div>
 <div class="form-group">
-   <label class="col-lg-2 control-label">Type the availible ports separated by commas. (No spaces!)</label>
+   <label class="control-label">Type the availible ports separated by commas. (No spaces!)</label>
    <input type="text" class="form-control" placeholder="25565,25626,26738">
 </div>
 <div class="btn-group" role="group">

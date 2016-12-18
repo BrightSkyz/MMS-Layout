2dbcb345d317d3d12f1612fa942bef24e1d99817
BrightSkyz <BrightSkyz@users.noreply.github.com>
2016-12-18 17:19:38 -0500

    Create 2console.php

---
+++ pages/server/2console.php
@@ -0,0 +1,37 @@
+<?php echo file_get_contents('../include/header.php'); ?>
+<h2>Console</h2>
+<div class="btn-group btn-group-justified">
+   <a href="2overview.php" class="btn btn-primary">Overview</a>
+   <a href="2console.php" class="btn btn-primary">Console</a>
+   <a href="2ftp.php" class="btn btn-primary">FTP</a>
+   <a href="2mymql.php" class="btn btn-primary">MySQL</a>
+   <a href="" class="btn btn-warning">Suspend Server</a>
+   <a href="" class="btn btn-danger">Delete Server</a>
+</div>
+<br>
+<div class="btn-group btn-group-justified">
+   <a href="#" class="btn btn-primary">Start</a>
+   <a href="#" class="btn btn-primary">Stop</a>
+   <a href="#" class="btn btn-primary">Restart</a>
+   <a href="#" class="btn btn-primary">Kill</a>
+</div>
+<br>
+<div class="form-group">
+   <label class="col-lg-2 control-label">Status</label>
+   <input disabled type="text" class="form-control" placeholder="Server Status" value="Online (9/25)">
+</div>
+<div class="panel panel-default">
+   <div class="panel-body">
+      <br><br><br><br><br><br><br><br><br>
+   </div>
+</div>
+<div class="form-group">
+   <div class="input-group">
+      <span class="input-group-addon">/</span>
+      <input type="text" class="form-control">
+      <span class="input-group-btn">
+         <button class="btn btn-default" type="button">Send</button>
+      </span>
+   </div>
+</div>
+<?php echo file_get_contents('../include/footer.php'); ?>

95ed360f03bd7070e06e01c38ce43885e7de4504
BrightSkyz <BrightSkyz@users.noreply.github.com>
2016-12-18 22:21:37 -0500

    Fixed players

--- pages/server/1overview.php
+++ pages/server/1overview.php
@@ -29,17 +29,13 @@
    <div class="col-lg-6">
       <div class="input-group">
          <span class="input-group-addon">
-            <input type="radio">
+            <input name="players" type="radio">
          </span>
          <input disabled type="text" class="form-control" value="Unlimited">
-      </div>
-   </div>
-   <div class="col-lg-6">
-      <div class="input-group">
          <span class="input-group-addon">
-            <input selected type="radio">
+            <input selected name="players" type="radio">
          </span>
-         <input type="text" class="form-control" value="10">
+         <input type="text" class="form-control" value="25">
       </div>
    </div>
 </div>

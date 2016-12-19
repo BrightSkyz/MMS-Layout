bcce637030e10b82d86c974d01e96610323622ee
BrightSkyz <BrightSkyz@users.noreply.github.com>
2016-12-19 16:00:35 -0500

    New layout

--- pages/server/2console.php
+++ pages/server/2console.php
@@ -1,12 +1,25 @@
 <?php echo file_get_contents('../include/header.php'); ?>
 <h2>Console</h2>
-<div class="btn-group btn-group-justified">
-   <a href="2overview.php" class="btn btn-primary">Overview</a>
-   <a href="2console.php" class="btn btn-primary">Console</a>
-   <a href="2ftp.php" class="btn btn-primary">FTP</a>
-   <a href="2mymql.php" class="btn btn-primary">MySQL</a>
+<div class="row">
+<div class="col-lg-2">
+<div class="btn-group-vertical">
+   <a href="1overview.php" class="btn btn-primary">Overview</a>
+   <a href="1console.php" class="btn btn-primary">Console</a>
+   <a href="1ftp.php" class="btn btn-primary">FTP</a>
+   <a href="1mymql.php" class="btn btn-primary">MySQL</a>
+   <br>
    <a href="" class="btn btn-warning">Suspend Server</a>
    <a href="" class="btn btn-danger">Delete Server</a>
+</div>
+</div>
+<br>
+<div class="col-lg-10">
+
+<div class="btn-group btn-group-justified">
+   <a href="" class="btn btn-primary">Start</a>
+   <a href="" class="btn btn-primary">Stop</a>
+   <a href="" class="btn btn-primary">Restart</a>
+   <a href="" class="btn btn-primary">Kill</a>
 </div>
 <br>
 <div class="btn-group btn-group-justified">
@@ -33,5 +46,5 @@
          <button class="btn btn-default" type="button">Send</button>
       </span>
    </div>
-</div>
+</div></div></div>
 <?php echo file_get_contents('../include/footer.php'); ?>

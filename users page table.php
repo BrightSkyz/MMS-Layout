c56e1b130035f691db3ee3a4624de95481a8fd8a
BrightSkyz <BrightSkyz@users.noreply.github.com>
2016-12-21 09:07:48 -0500

    Just the table

---
+++ users page table.php
@@ -0,0 +1,18 @@
+<div class="table-responsive">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Type</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                 <td>1</td>
+                 <td><a href="/admin/users/id/1">Admin</td>
+                 <td>superuser</td>
+             </tr>
+         </tbody>
+     </table>
+   </div>
